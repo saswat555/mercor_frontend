96ccde7141257cc89218444bc3dab90f93abad3a
saswat555 <sapa19is@cmrit.ac.in>
2024-02-18 19:13:26 +0530

    fixed build issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState} from 'react';
-import axios from 'axios';
+import React from 'react';
 import 'tailwindcss/tailwind.css';
 import './index.css';
 import ChatComponent from './ChatComponent1';
