968364e14ecd2e2edd30a1ea7750c75f08610dfd
saswat555 <sapa19is@cmrit.ac.in>
2024-02-18 15:49:06 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ProfileCard from './ProfileCard';
+import 'tailwindcss/tailwind.css';
+import './index.css';
+import ChatComponent from './ChatComponent1';
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [profiles, setProfiles] = useState([]);
+
+  // Function to handle sending messages as queries
+  const handleSendMessage = async (message) => {
+    setLoading(true); // Start loading
+
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/search', {
+        query: message, // Use the message as the query
+      });
+      setProfiles(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false); // Stop loading
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ChatComponent/>
+    </>
   );
 }
 
